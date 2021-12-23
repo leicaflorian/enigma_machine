eec0fce60f0392d2058152fe88d2d8d7d816c37d
leicaflorian <florian.leica@gmail.com>
2021-12-23 23:55:27 +0100

    Creates the LightBoard

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,27 @@
-<script setup>
-  import { Plugboard } from './composables/Plugboard';
-  import { Rotor } from './composables/Rotor';
-  import { Enigma } from './composables/Enigma';
-
-  const enigma = new Enigma(['I', 'IV', 'II'], ["B", "X", "A"], "B");
-
-  window.addEventListener("keydown", function (e) {
-    console.log("IN", e.key, "OUT", enigma.input(e.key));
-  });
-  console.log(enigma);
-
-</script>
-
 <template>
-  <!--  <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello Vue 3 + Vite" />-->
+  <div class="container">
+    <LightsBoard :value="resultLetter"></LightsBoard>
+  </div>
 </template>
 
-<style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+<script setup lang="ts">
+  import LightsBoard from './components/LightsBoard.vue';
+  import { Enigma } from './composables/Enigma';
+  import { ref } from 'vue';
+
+  const resultLetter = ref(null);
+  const enigma = new Enigma(['I', 'IV', 'II'], ["B", "I", "A"], "B");
+
+  enigma.addEventListener("encryption", (e: CustomEvent<string>) => {
+
+    resultLetter.value = e.detail
+  })
+
+  // window.enigma = enigma;
+</script>
+
+
+<style lang="scss">
+  @import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 </style>
---
+++ src/components/LightsBoard.vue
@@ -0,0 +1,57 @@
+<template>
+  <template v-for="row in rows">
+    <div class="row mb-2">
+      <div class="col" v-for="letter in row">
+        <div class="letter" :class="{'active': value === letter}">{{ letter }}</div>
+      </div>
+    </div>
+  </template>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: "LightsBoard",
+    props: {
+      value: String
+    },
+    setup () {
+      const rows = ["QWERTZUIO", "ASDFGHJK", "PYXCVBNML"]
+
+      return {
+        rows
+      }
+    }
+  });
+</script>
+
+<style scoped lang="scss">
+  ::v-deep .letter {
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: solid 1px black;
+    border-radius: 50%;
+    margin: auto;
+    transition: background-color .3s;
+
+    &.active {
+      animation: activate 1s;
+    }
+
+    @keyframes activate {
+      0% {
+        background-color: transparent;
+      }
+      25% {
+        background-color: yellow;
+      }
+      100% {
+        background-color: transparent;
+      }
+    }
+  }
+</style>
