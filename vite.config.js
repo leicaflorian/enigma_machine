cf1d708389497e1855be40e7d3c3692084eafda4
leicaflorian <florian.leica@gmail.com>
2021-12-24 18:27:39 +0100

    Fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir:  process.env.NODE_ENV === 'production'
+  base:  process.env.NODE_ENV === 'production'
   ? '/enigma_machine/'
   : '/'
 })
